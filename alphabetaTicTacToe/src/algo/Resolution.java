16f8e71380c4242034475c40bc98b2f1cf9432b6
luna <yannbrun1507@outlook.fr>
2025-09-25 14:55:07 +0200

    refactored Resolution

--- alphabetaTicTacToe/src/algo/Resolution.java
+++ alphabetaTicTacToe/src/algo/Resolution.java
@@ -10,6 +10,13 @@ import model.Situation;
 public class Resolution {
 
     /**
+     * Explicitly define a constructor to throw an error if instantiated.
+     */
+    private Resolution() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
+    /**
      * Alpha-beta function, determines the value of the node/situation s.
      * To retrieve the situation to take, scan the list of successors of s
      * and take the first situation with the same estimate.
@@ -19,16 +26,25 @@ public class Resolution {
      * @return estimate of the situation s based on the opponent's play
      */
     public static double alphaBeta(Situation s, double alpha, double beta) {
-        double bound, value;
+        double bound;
+        double value;
         if (s.isLeaf() || s.isClose()) {
             return s.getH();
         }
 
-        if (s.isMax()) {
-            ArrayList<Situation> successors = s.getSuccessors();
+        ArrayList<Situation> successors = s.getSuccessors();
+
+
+
+
             bound = alpha;
             for (Situation successor : successors) {
-                value = alphaBeta(successor, bound, beta);
+                if (s.isMax()) {
+                    value = alphaBeta(successor, bound, beta);
+                }
+                else {
+                    value = alphaBeta(successor, alpha, bound);
+                }
                 successor.setH(value);
                 if (value > bound) {
                     bound = value;
@@ -37,20 +53,6 @@ public class Resolution {
                     return bound;
                 }
             }
-        } else {
-            ArrayList<Situation> successors = s.getSuccessors();
-            bound = beta;
-            for (Situation successor : successors) {
-                value = alphaBeta(successor, alpha, bound);
-                successor.setH(value);
-                if (value < bound) {
-                    bound = value;
-                }
-                if (value <= alpha) {
-                    return bound;
-                }
-            }
-        }
         return bound;
     }
 }
